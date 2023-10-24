0279690d845e502f797c27a858081d9a9ec33896
sulaiman0313 <sulemanakhtar57@gmail.com>
2023-10-24 11:55:28 +0530

    error number

--- app.js
+++ app.js
@@ -1,7 +1,28 @@
 
-function sum(x){
-    for(let i=1;i<=x;i++){
-        console.log(i)
-    }
-}
-sum(5)
+// function sum(x){
+//     for(let i=1;i<=x;i++){
+//         console.log(i)
+//     }
+// }
+// sum(5)
+
+
+const input=document.querySelector('#input')
+const btn=document.querySelector('#btn')
+const sum=document.querySelector('.sum')
+
+btn.addEventListener('click',()=>{
+    
+   
+    const InpuValue=input.value;
+
+        const h1=document.createElement('h1')
+
+        for(let i=0;i<=InpuValue;i++){
+           console.log(h1.innerHTML=i)
+        }
+
+    sum.appendChild(h1)
+
+    
+})
